414ab5d56b2724be25ad01fc8604391599b91a0a
IvanEh <ivanehreshi@gmail.com>
2016-04-10 21:53:10 +0300

    Create a Strings entity for storing strings

---
+++ src/main/java/com/gmail/at/ivanehreshi/Strings.java
@@ -0,0 +1,12 @@
+package com.gmail.at.ivanehreshi;
+
+public class Strings {
+
+    public static class Menu {
+        public static final String FILE = "Файл";
+        public static final String HELP = "Допомога";
+        public static final String CLOSE = "Закрити";
+        public static final String ABOUT = "Про програму";
+    }
+
+}

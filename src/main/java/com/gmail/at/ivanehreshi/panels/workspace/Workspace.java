087f904d497e7977aecd20af64d17db03910c78d
IvanEh <ivanehreshi@gmail.com>
2016-04-13 23:28:37 +0300

    Add some sample code to Workspace

--- src/main/java/com/gmail/at/ivanehreshi/panels/workspace/Workspace.java
+++ src/main/java/com/gmail/at/ivanehreshi/panels/workspace/Workspace.java
@@ -1,10 +1,45 @@
 package com.gmail.at.ivanehreshi.panels.workspace;
 
+import com.gmail.at.ivanehreshi.customui.MindMapDrawer;
+import com.gmail.at.ivanehreshi.customui.NodeView;
+import com.gmail.at.ivanehreshi.models.NodeModel;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.geom.Point2D;
 
 public class Workspace extends JPanel {
     public Workspace() {
         this.setBorder(BorderFactory.createLineBorder(new Color(0, 255, 255)));
+        initGui();
+        initDebugGui();
+    }
+
+    private void initDebugGui() {
+        this.setLayout(new BorderLayout());
+        NodeModel model = new NodeModel("Root");
+        NodeModel model1 = new NodeModel("Node1", model);
+        NodeModel model2 = new NodeModel("Node2", model);
+        NodeModel model3 = new NodeModel("Node3", model);
+        NodeModel model3_1 = new NodeModel("Node3_1", model3);
+        model3_1.setPolarCoord(0.3, Math.toRadians(30));
+
+        model1.setPolarCoord(0.2, Math.toRadians(-10));
+        model2.setPolarCoord(0.2, Math.toRadians(45));
+        model3.setPolarCoord(0.2, Math.toRadians(45));
+
+
+        model.addNode(model1);
+        model.addNode(model2);
+        model.addNode(model3);
+        model3.addNode(model3_1);
+        MindMapDrawer mindMapDrawer = new MindMapDrawer(model);
+
+        mindMapDrawer.setVisible(true);
+        this.add(mindMapDrawer, BorderLayout.CENTER);
+
+    }
+
+    private void initGui() {
     }
 }

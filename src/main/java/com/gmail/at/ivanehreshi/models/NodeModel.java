bb2f3973fd527b916d996ab4bd459e2befe9d68c
IvanEh <ivanehreshi@gmail.com>
2016-04-13 23:27:42 +0300

    Update model to incapsulate position information

--- src/main/java/com/gmail/at/ivanehreshi/models/NodeModel.java
+++ src/main/java/com/gmail/at/ivanehreshi/models/NodeModel.java
@@ -5,7 +5,9 @@ import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 import javax.swing.*;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
+import java.awt.*;
 import java.awt.event.ActionListener;
+import java.awt.geom.Point2D;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Iterator;
@@ -17,6 +19,8 @@ public class NodeModel implements Iterable<NodeModel>{
     private ArrayList<NodeModel> nodes;
     private ArrayList<ChangeListener> changeListeners;
     private NodeModel parentNode;
+    private double phi;
+    private double rhoNorm;
 
     public NodeModel(String title) {
         this.title = title;
@@ -93,12 +97,84 @@ public class NodeModel implements Iterable<NodeModel>{
         return nodes;
     }
 
+    public double getPhi() {
+        return phi;
+    }
+
+    public void setPhi(double phi) {
+        this.phi = phi;
+    }
+
+    public double getRhoNorm() {
+        return rhoNorm;
+    }
+
+    public void setRhoNorm(double rhoNorm) {
+        this.rhoNorm = rhoNorm;
+    }
+
+    public Point computePosition(int width, int height) {
+        double rho = computeMagnitude(width);
+        double phi = computeStdTrigAngle();
+        double x = rho*Math.cos(phi);
+        double y = -rho*Math.sin(phi);
+        return new Point((int)x, (int)y);
+    }
+
     protected void fireChangeEvent(ChangeEvent event) {
         changeListeners.forEach((listener) -> listener.stateChanged(event) );
     }
 
+    // TODO: optimize
+    public int getIndex() {
+        if (parentNode == null) {
+            return 0;
+        }
+
+        return parentNode.getNodes().indexOf(this);
+    }
+
+    public boolean isRootNode() {
+        return parentNode == null;
+    }
+
     public NodeModelIterator iterator() {
         return new NodeModelIterator();
+    }
+
+    public NodeModel getParent() {
+        return parentNode;
+    }
+
+    public NodeModel getPrev() {
+        if(isRootNode()) {
+            return null;
+        }
+
+        int index = getIndex();
+        if(index != 0) {
+            return getParent().getNodes().get(index - 1);
+        }
+
+        return null;
+    }
+
+    public void setPolarCoord(double rhoNorm, double phi) {
+        this.rhoNorm = rhoNorm;
+        this.phi = phi;
+    }
+    @Override
+    public String toString() {
+        return this.title;
+    }
+
+    public double computeStdTrigAngle() {
+        return Math.PI / 2 - phi;
+    }
+
+    public double computeMagnitude(int width) {
+        double rho = rhoNorm * width;
+        return rho;
     }
 
     public static class NodeModelChangeEvent extends ChangeEvent {
@@ -149,10 +225,13 @@ public class NodeModel implements Iterable<NodeModel>{
             }
 
             NodeModel ret = nodes.pop();
-            for(NodeModel node: ret.getNodes()) {
-                nodes.push(node);
+
+            for (int i = ret.nodes.size() - 1; i >= 0; i--) {
+                nodes.push(ret.nodes.get(i));
             }
+
             return ret;
         }
     }
+
 }
